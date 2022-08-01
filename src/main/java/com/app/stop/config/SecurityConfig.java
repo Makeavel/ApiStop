420be11f4d47ea67c5cb24d55240b5b5967d2550
Paulo <dpseutroco@gmail.com>
2022-08-01 19:54:59 -0300

    security

--- src/main/java/com/app/stop/config/SecurityConfig.java
+++ src/main/java/com/app/stop/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
         @Override
         protected void configure( HttpSecurity http ) throws Exception {
             http
+                    .cors()
+                    .and()
                     .csrf().disable()
                     .authorizeRequests()
                     .antMatchers("/api/stop/**")
